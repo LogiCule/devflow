9f93f0e6d682c309b00bd05e4f195d10766a0a8b
Rahul Dubey <rahuldubey7398@gmail.com>
2025-05-28 22:05:59 +0530

    dark class

--- components/search/LocalSearch.tsx
+++ components/search/LocalSearch.tsx
@@ -18,7 +18,7 @@ const LocalSearch = ({
   route,
   imgSrc,
   placeholder,
-  otherClasses="",
+  otherClasses = "",
 }: searchProps) => {
   const searchParams = useSearchParams();
   const router = useRouter();
@@ -50,7 +50,7 @@ const LocalSearch = ({
 
   return (
     <div
-      className={`background-light800_dark400 flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
+      className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
     >
       <Image
         src={imgSrc}
