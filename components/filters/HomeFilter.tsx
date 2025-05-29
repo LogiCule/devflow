d920974c3e5442f264b790c05a11ddef60b43dd6
Rahul Dubey <rahuldubey7398@gmail.com>
2025-05-29 12:53:51 +0530

    home filter

--- components/filters/HomeFilter.tsx
+++ components/filters/HomeFilter.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/url";
+import { cn } from "@/lib/utils";
+
 import { Button } from "../ui/button";
 
 const filters = [
@@ -8,10 +16,43 @@ const filters = [
 ];
 
 const HomeFilter = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const filterParams = searchParams.get("filter") || "";
+
+  const handleFilterChange = (filter: string, isSelected: boolean) => {
+    if (!isSelected) {
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: filter,
+      });
+      router.push(newUrl, { scroll: false });
+    } else {
+      const newUrl = removeKeysFromQuery({
+        params: searchParams.toString(),
+        keysToRemove: ["filter"],
+      });
+      router.push(newUrl, { scroll: false });
+    }
+  };
   return (
-    <div>
+    <div className="mt-10 hidden flex-wrap gap-3 sm:flex">
       {filters.map((filter) => (
-        <Button key={filter.name}></Button>
+        <Button
+          onClick={() =>
+            handleFilterChange(filter.value, filterParams === filter.value)
+          }
+          className={cn(
+            `body-medium rounded-lg px-6 py-3 capitalize shadow-none`,
+            filterParams === filter.value
+              ? "bg-primary-100 text-primary-500 hover:bg-primary-100 dark:bg-dark-400 dark:text-primary-500 dark:hover:bg-dark-400"
+              : "bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300"
+          )}
+          key={filter.name}
+        >
+          {filter.name}
+        </Button>
       ))}
     </div>
   );
