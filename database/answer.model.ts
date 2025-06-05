fe6f272938ed602787c41f7fccded86f59eaefd1
Rahul Dubey <rahuldubey7398@gmail.com>
2025-06-05 17:03:46 +0530

    extended models

--- database/answer.model.ts
+++ database/answer.model.ts
@@ -1,22 +1,27 @@
-import { model, models, Schema, Types } from "mongoose";
+import { Schema, models, model, Types, Document } from "mongoose";
 
 export interface IAnswer {
   author: Types.ObjectId;
   question: Types.ObjectId;
-  upvotes?: number;
-  downvotes?: number;
   content: string;
+  upvotes: number;
+  downvotes: number;
 }
 
+export interface IAnswerDoc extends IAnswer, Document {}
 const AnswerSchema = new Schema<IAnswer>(
   {
     author: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
+    question: {
+      type: Schema.Types.ObjectId,
+      ref: "Question",
+      required: true,
+    },
+    content: { type: String, required: true },
     upvotes: { type: Number, default: 0 },
     downvotes: { type: Number, default: 0 },
-    content: { type: String, required: true },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 const Answer = models?.Answer || model<IAnswer>("Answer", AnswerSchema);
